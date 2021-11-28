7fce949258df2514c3a4a0adea4170a735df1ffd
AndreyBernacky <dronskmne@gmail.com>
2021-11-28 12:40:08 +0300

    5. add name 1

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
 let searchInput = document.querySelector(".form--txt");
 let btnInput = document.querySelector(".form--submit");
 
+let nameAge = [
+    {
+        name: "Valera",
+        age: "20",
+    },
+    {
+        name: "Andrey",
+        age: "23",
+    },
+    {
+        name: "Igor",
+        age: "25",
+    },
+    {
+        name: "Kolya",
+        age: "30",
+    },
+];
+
 btnInput.addEventListener('click', function(){
     inputValue = searchInput.value == "google" ? "Yandex круче. Но это не точно" : searchInput.value
-    alert(inputValue)
+    nameAge1 = nameAge[0].name
+
+    alert(inputValue + nameAge1)
 })
