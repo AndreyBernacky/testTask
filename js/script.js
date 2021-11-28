6f289982dd8c98d697e68928cdf5dc9a255c8c5d
AndreyBernacky <dronskmne@gmail.com>
2021-11-28 12:55:28 +0300

    7. find max and min array elements

--- js/script.js
+++ js/script.js
@@ -31,4 +31,17 @@ function superSum(a,b){
     alert(a + b)
 }
 
-btnInput.addEventListener('click', superSum(3,4))
+arr = [12,6,3,4,65,23,7,9,8]
+
+function minmaxArr(arr){
+    let min = arr[0]
+    let max = arr[0]
+    for(i = 0; i < arr.length; i++){
+        min = min < arr[i] ? min : arr[i]
+        max = max > arr[i] ? max : arr[i]
+    }
+
+    alert("min = " + min + "\n" + "max = " + max)
+}
+
+btnInput.addEventListener('click', minmaxArr(arr))
