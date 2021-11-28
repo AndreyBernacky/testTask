5cedf0e24ea16cfb124bbe87e4d7562e7a335f00
AndreyBernacky <dronskmne@gmail.com>
2021-11-28 12:22:37 +0300

    3. alert input.value

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let searchInput = document.querySelector(".form--txt");
+let btnInput = document.querySelector(".form--submit");
+
+btnInput.addEventListener('click', function(){
+    alert(searchInput.value)
+})
