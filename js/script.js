776a4779d28968ea709e85e7a2c69479d652590a
AndreyBernacky <dronskmne@gmail.com>
2021-11-28 12:26:34 +0300

    4. change 'google' on 'Yandex круче. Но это не точно'

--- js/script.js
+++ js/script.js
@@ -2,5 +2,6 @@ let searchInput = document.querySelector(".form--txt");
 let btnInput = document.querySelector(".form--submit");
 
 btnInput.addEventListener('click', function(){
-    alert(searchInput.value)
+    inputValue = searchInput.value == "google" ? "Yandex круче. Но это не точно" : searchInput.value
+    alert(inputValue)
 })
